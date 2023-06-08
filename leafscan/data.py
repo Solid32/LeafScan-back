8d7e8efd3c67238f1234da39f2c674a0de40c36a
Guillaume Chinzi <guillaume.chinzi@hotmail.be>
2023-06-08 14:06:55 +0200

    conflit incoming

---
+++ leafscan/data.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+def download_data(path=None, batch_size=32, image_size=(256,256),shuffle=True, testratio=0.9, valratio=0.8) :
+
+    if path == None :
+        data_dir = '../raw_data'
+    else :
+        data_dir = path
+
+    dataset = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        batch_size=batch_size,
+        image_size=image_size,
+        shuffle=shuffle,
+        seed=42 )
+    train_val_ds = dataset.take(round(len(dataset) * testratio))  # 90% pour l'entraînement
+    test_ds = dataset.skip(round(len(dataset) * testratio))  # 10% pour le test
+
+    train_ds = dataset.take(round(len(train_val_ds) * valratio))  # 90% pour l'entraînement
+    val_ds = dataset.skip(round(len(train_val_ds) * valratio))  # 10% pour le test
+
+    print("✅ data loaded")
+
+    return train_ds , test_ds , val_ds
---
+++ leafscan/model.py
@@ -0,0 +1,72 @@
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+from keras import Model, Sequential, layers, regularizers, optimizers
+from keras.callbacks import EarlyStopping
+
+def initialize_model():
+    mirrored_strategy = tf.distribute.MirroredStrategy()
+    with mirrored_strategy.scope():
+    #-->
+        model = tf.keras.Sequential([
+        tf.keras.layers.Rescaling(1./255, input_shape=(256,256,3)),
+        tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+        tf.keras.layers.CenterCrop(height=224, width=224),
+        tf.keras.layers.RandomRotation(0.2),
+        tf.keras.layers.Conv2D(64, 4, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(246, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(39, activation='softmax')
+        ])
+    print("✅ Model compiled")
+    return model
+
+def compile(model, lr_rate = 0.0015, dc_steps = 2000, dc_rate = 0.9):
+    initial_learning_rate = lr_rate
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate,
+    decay_steps=dc_steps,
+    decay_rate=dc_rate
+)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+
+    model.compile(optimizer=optimizer,
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+
+    print("✅ Model compiled")
+
+    return model
+def train(model, train_ds , val_ds, epochs = 20, patience = 5):
+    es = EarlyStopping(
+        patience=patience,
+        restore_best_weights=True,
+        verbose=1
+    )
+
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs,
+        callbacks=[es],
+        verbose=1
+    )
+
+    print(f"✅ Model trained on {train_ds.cardinality()} rows with min val MAE: {round(np.min(history.history['accuracy']), 2)}")
+
+    return model, history
+
+def evaluate(model, test_ds):
+
+    value = model.evaluate(test_ds)
+    return value
